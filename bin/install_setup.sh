79826fcfdf0358c4bb93ad844f6b347486392f2c
crisfbazz <crisfbazz@gmail.com>
2025-09-30 19:18:12 -0300

    added tzcode package in the conda env and intalling the tzdata package in the system directly using the install_setup script

--- bin/install_setup.sh
+++ bin/install_setup.sh
@@ -1,4 +1,6 @@
 # install nodejs packages not present in conda and audit the installation
 npm install shelljs@0.8.4 commander@5.1.0 
+# install tzdata - internal R dependency
+apt-get install -y tzdata 
 # execute the np3 update and setup run
 cd NP3_MS_Workflow && node np3_workflow.js setup && cd -
