8d8b00d810fac0e122a396f8240eb6cad6b8f78f
crisfbazz <crisfbazz@gmail.com>
2025-12-02 10:06:41 -0300

    bin/install_setup: updated the NP3_MS_Workflow relative path to the current repo main folder

--- bin/install_setup.sh
+++ bin/install_setup.sh
@@ -1,6 +1,6 @@
-# install nodejs packages not present in conda and audit the installation
+# install nodejs packages not present in conda
 npm install shelljs@0.8.4 commander@5.1.0 
 # install tzdata - internal R dependency
 apt-get install -y tzdata 
 # execute the np3 update and setup run
-cd NP3_MS_Workflow && node np3_workflow.js setup && cd -
+cd bin/NP3_MS_Workflow && node np3_workflow.js setup && cd -
