1b3a76f96e4869119c58197b19694f53477a24f9
Mingxun Wang <mwang87@gmail.com>
2025-12-03 09:52:03 -0800

    cleaning up dep

--- bin/install_setup.sh
+++ bin/install_setup.sh
@@ -1,6 +1,5 @@
 # install nodejs packages not present in conda
 npm install shelljs@0.8.4 commander@5.1.0 
-# install tzdata - internal R dependency
-apt-get install -y tzdata 
+
 # execute the np3 update and setup run
 cd bin/NP3_MS_Workflow && node np3_workflow.js setup && cd -
